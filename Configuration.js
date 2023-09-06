ffcc5b4aff332dbf35b7873e8e2af854fcda1282
ayushchikumd <a86180214@gmail.com>
2023-09-06 15:25:46 +0530

    𝐀𝐲𝐮𝐬𝐡 𝐩𝐚𝐧𝐝𝐞𝐲🚀

---
+++ Configuration.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+global.available = true;
+global.autoReadGc = true;
+global.autoReadAll = false;
+global.antitags = true;
+
+global.Owner = ["919931122319"];
+global.OwnerNumber = ["919931122319"];
+global.ownertag = ["919931122319"];
+global.BotName = "Maria-Md";
+global.OwnerName = "Ayush pandey";
+global.support = "https://chat.whatsapp.com/FoS7pSPtfMqBuoireK4aAJ"
+//global.sessionName = "session";
+global.themeemoji = '🍭';
+global.packname = 'Maria';
+global.author = 'Ayush';
+global.thum = fs.readFileSync("./Assets/pic7.jpg") 
+
+//   Prefix  //
+global.prefa = ["!"];
+
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+global.mess = {
+    pm: ' 📍Bot wont work in private darling📍 \n\n https://chat.whatsapp.com/FoS7pSPtfMqBuoireK4aAJ',
+    promote: 'promote success...',
+    jobdone: 'Job done...',
+    useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
+    botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
+    botowner: 'Only my *Owner* can use this command, Baka!',
+    grouponly: 'This command is only made for *Groups*, Baka!',
+    privateonly: 'This command is only made for *Private Chat*, Baka!',
+    botonly: 'Only the *Bot itself* can use this command!',
+    waiting: 'Chotto Matte...',
+    nolink: 'Please provide me *link*, Baka!',
+    error: 'An error occurd!',
+    banned: 'You are *Banned* fron using commands!',
+    bangc: 'This Group is *Banned* from using Commands!',
+    nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+    
+}
+
+global.limitawal = {
+  premium: "Infinity",
+  free: 2,
+  monayawal: 1000,
+};
+
+global.limitawal = {
+  rakyat: "Infinity",
+  free: 100,
+};
+
+global.APIs = {
+  zenz: "https://zenzapis.xyz",
+};
+global.APIKeys = {
+  "https://zenzapis.xyz": "5d1197db351b",
+};
